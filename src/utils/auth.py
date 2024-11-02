690b1855ef870512a6da9e7e31fce70090c4676e
samarth1029 <87478607+samarth1029@users.noreply.github.com>
2024-11-02 21:18:43 +0530

    create auth for hf and wandb

---
+++ src/utils/auth.py
@@ -0,0 +1,18 @@
+from huggingface_hub import login
+import wandb
+from dotenv import load_dotenv
+import os
+
+
+class Authenticator:
+    def __init__(self):
+        load_dotenv()
+        self.hf_token = os.getenv("HUGGING_FACE_TOKEN")
+        self.wandb_token = os.getenv("WANDB_TOKEN")
+
+    def authenticate(self):
+        if not self.hf_token or not self.wandb_token:
+            raise ValueError("HUGGING_FACE_TOKEN or WANDB_TOKEN is not set in the .env file.")
+        login(token=self.hf_token)
+        wandb.login(key=self.wandb_token)
+        print("Authenticated with Hugging Face and WandB")
